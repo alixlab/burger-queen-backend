2b820deba7d931b4ac5934d7588c05bb0492b913
alixlab <alicelabarca@gmail.com>
2019-07-05 14:47:52 -0300

    Burger Queen

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const db = require('./models/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const PORT = process.env.PORT || 4567;
 app.listen(PORT, console.log(`server started on port ${PORT}`));
@@ -10,5 +12,6 @@ app.use(express.urlencoded({extended: true}));
 app.use("/users", require('./routes/user'));
 app.use("/products", require('./routes/products'));
 app.use("/orders", require('./routes/orders'));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 db.sequelize.sync();
